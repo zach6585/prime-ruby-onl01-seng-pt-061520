89e458fef37ccc8bce2f55fcb131a7b59175221b
zachary mines <zmines@wesleyan.edu>
2020-06-24 01:21:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  i = 2
+  if integer == 1 or integer == 0 
+    return false
+  elsif integer < 0 
+    return false
+  end 
+  while i < integer
+    if integer % i == 0 
+      return false 
+    end 
+    i +=1
+  end 
+  return true  
+end 
